22ab02d8a89de12b9020c9af116343ffd764d8c4
Andrey Dantas <andreydantasvf@gmail.com>
2025-10-04 21:40:34 -0300

    feat: Configure CORS with allowed origins from environment variable

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routes/router.js');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+const allowedOriginsEnv = process.env.ALLOWED_ORIGIN || '*';
+
+const corsOptions = {
+    origin: allowedOriginsEnv
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT;
 
