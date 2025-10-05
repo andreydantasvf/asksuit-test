70d27c72fcef943cbfcf23f043741707eeec5feb
Andrey Dantas <andreydantasvf@gmail.com>
2025-10-04 21:30:33 -0300

    feat: Add error handling middleware to the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./routes/router.js');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 const port = process.env.PORT;
 
 app.use('/', router);
+app.use(errorHandler);
 app.listen(port || 8080, () => {
     console.log(`Listening on port ${port}`);
 });
